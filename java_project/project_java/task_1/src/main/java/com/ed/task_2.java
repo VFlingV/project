bdd8d60fe40783a3bc4fec057ffc63b422f4b436
Eduard <Nollkol>
2022-10-26 16:54:34 +0500

    new_2

--- java_project/project_java/task_1/src/main/java/com/ed/task_2.java
+++ java_project/project_java/task_1/src/main/java/com/ed/task_2.java
@@ -1,9 +1,26 @@
 package com.ed;
 
+
+/**
+ * Hello world!
+ *
+ */
 public class task_2 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hell222o World!" );
+        int i;
+        for (int n = 1; n<101; n++){
+            i = 0;
+
+            for (int j = 1; j <= n; j++){
+                if (n % j == 0)
+                j++;
+            }
+            if (i <= 2 & i > 1) 
+                System.out.println(n);
+        }
+
+
     }
 }
